1149260cd7efe9df9d1c1dc9e982d7718a2d8868
Lucid-Synth <uacode75@gmail.com>
2026-03-26 23:44:13 +0530

    Created a basic dashboard page with upload button

---
+++ app/components/Reusable/Logo.tsx
@@ -0,0 +1,11 @@
+
+const Logo = () => {
+  return (
+    <div className="lg:hidden flex items-center gap-2">
+        <span className="flex h-7 w-7 items-center justify-center rounded-lg bg-linear-to-br from-[#a78bfa] to-[#6366f1] text-white text-xs font-black">Z</span>
+        <span className="font-semibold text-white text-sm">Zenvy</span>
+      </div>
+  )
+}
+
+export default Logo
